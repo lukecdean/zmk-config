172287e17a99d2aa368b1c2ec1819c3fc13128bb
lukecdean <ldean35@gatech.edu>
2023-09-07 18:46:03 -0400

    consolodating corne repo with main zmk repo

---
+++ config/keymap.h
@@ -0,0 +1,25 @@
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/outputs.h>
+
+#define _BAS 0
+#define _SFT 1
+#define _SM1 2
+#define _SM2 3
+#define _NNV 4
+#define _ANV 5
+#define _I3W 6
+#define _BLE 7
+
+#define _L_KEYS 0 1 2 3 4 5 12 13 14 15 16 17 24 25 26 27 28 29 36 37 38
+#define _R_KEYS 6 7 8 9 10 11 18 19 20 21 22 23 30 31 32 33 34 35 39 40 41
+
+/*
+lyr { bindings = <
+    &trans &trans &trans &trans &trans &trans   &trans &trans &trans &trans &trans &trans
+    &trans &trans &trans &trans &trans &trans   &trans &trans &trans &trans &trans &trans
+    &trans &trans &trans &trans &trans &trans   &trans &trans &trans &trans &trans &trans
+                         &trans &trans &trans   &trans &trans &trans
+>; };
+*/
