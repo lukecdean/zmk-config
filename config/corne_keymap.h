499627da90a75512ff838c292fe9b2ccb45c89f0
lukecdean <ldean35@gatech.edu>
2024-02-18 16:55:46 -0500

    transforming current corne km to cradio km

--- config/corne_keymap.h
+++ config/corne_keymap.h
@@ -9,10 +9,11 @@
 #define _SM2 3
 #define _NNV 4
 #define _ANV 5
-#define _BLE 6
+#define _I3W 6
+#define _BLE 7
 
-#define _L_KEYS 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24 30 31
-#define _R_KEYS 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29 32 33
+#define _L_KEYS 0 1 2 3 4 5 12 13 14 15 16 17 24 25 26 27 28 29 36 37 38
+#define _R_KEYS 6 7 8 9 10 11 18 19 20 21 22 23 30 31 32 33 34 35 39 40 41
 
 /*
 lyr { bindings = <
