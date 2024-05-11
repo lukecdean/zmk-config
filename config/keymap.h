bbfd1a78a58cd452b8d3a687cb3fd20faf501870
lukecdean <ldean35@gatech.edu>
2024-05-10 21:10:14 -0400

    adding gaming layer (no hrm etc)

--- config/keymap.h
+++ config/keymap.h
@@ -10,6 +10,7 @@
 #define _NNV 4
 #define _ANV 5
 #define _BLE 6
+#define _GAM 7
 
 #define _L_KEYS 0 1 2 3 4 10 11 12 13 14 20 21 22 23 24 30 31
 #define _R_KEYS 5 6 7 8 9 15 16 17 18 19 25 26 27 28 29 32 33
